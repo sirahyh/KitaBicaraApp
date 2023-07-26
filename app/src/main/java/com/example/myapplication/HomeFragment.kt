27de6fb2f92f15ece4f928548e38b69ff2d9f243
Sitti Rahma <sirah.yh@gmail.com>
2023-07-26 21:37:51 +0800

    Tutorial Page

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 
 
 class HomeFragment : Fragment() {
@@ -18,6 +20,31 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        // return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val btnRekam = view.findViewById<ImageButton>(R.id.btn_tutorial_rekam)
+        val btnTulis = view.findViewById<ImageButton>(R.id.btn_tutorial_tulis)
+
+        // Set on click listener for btnRekam
+        btnRekam.setOnClickListener {
+            val rekamTutorialFragment = RekamTutorialFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, rekamTutorialFragment)
+                .addToBackStack(null) // Optional: Add to back stack
+                .commit()
+        }
+
+        // Set on click listener for btnTulis
+        btnTulis.setOnClickListener {
+            val tulisTutorialFragment = TulisTutorialFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, tulisTutorialFragment)
+                .addToBackStack(null) // Optional: Add to back stack
+                .commit()
+        }
+
+        return view
     }
 }
---
+++ app/src/main/java/com/example/myapplication/RekamTutorialFragment.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+
+class RekamTutorialFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_rekam_tutorial, container, false)
+
+        val backBtn = view.findViewById<ImageButton>(R.id.imageButton)
+
+        backBtn.setOnClickListener {
+            val homeFragment = HomeFragment()
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, homeFragment)
+                .addToBackStack(null) // Optional: Add to back stack
+                .commit()
+        }
+        return view
+    }
+
+
+}
